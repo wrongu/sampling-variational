e822d431774533b4cd468d68cb421860d2a9c570
wrongu <richard.d.lange.13@dartmouth.edu>
2021-08-31 17:30:47 -0400

    added log_prob_banana and log_prob_cigar to distributions.py. still todo: adding other toy/demo distributions

---
+++ distributions.py
@@ -0,0 +1,39 @@
+import torch
+
+
+def log_prob_banana(xy):
+    x, y = xy[0], xy[1]
+    # Easier to read version: -(y-(x/2)^2)^2 - (x/2)^2
+    return -x**4/8+y*x**2-2*y**2-x**2/4
+
+
+def log_prob_cigar(xy, c=.99):
+    x, y = xy[0], xy[1]
+    # Compute precision terms for covariance matrix [[1,c],[c,1]], where precision will be [[a,b],[b,a]]
+    a = 1./(1. - c*c)
+    b = -c/(1. - c*c)
+    return -0.5*(a*x**2 + a*y**2 + 2*b*x*y)
+
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    x = torch.linspace(-4, 4)
+    xx, yy = torch.meshgrid(x, x)
+    xy = torch.stack([xx.flatten(), yy.flatten()], dim=0)
+
+    def plot_helper(log_prob_fn):
+        lp = log_prob_fn(xy)
+        p = (lp - lp.max()).exp().reshape(xx.size())
+        plt.contourf(xx, yy, p, origin='lower')
+        plt.xlim([min(x), max(x)])
+        plt.ylim([min(x), max(x)])
+
+    plt.figure(figsize=(8, 4))
+    plt.subplot(1, 2, 1)
+    plot_helper(log_prob_banana)
+    plt.title('banana')
+    plt.subplot(1, 2, 2)
+    plot_helper(log_prob_cigar)
+    plt.title('cigar')
+    plt.show()
