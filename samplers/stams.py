25f10cae720e36fa6fe5aabab0a300f3fca8b08b
wrongu <richard.d.lange.13@dartmouth.edu>
2021-09-02 09:00:44 -0400

    Tweak to stams langevin -- in terms of theta instead of q

--- samplers/stams.py
+++ samplers/stams.py
@@ -15,7 +15,9 @@ def stams_mvn_langevin(log_p, lam_kl, q_init, n_samples=1000, burn_in=100, n_kl_
     samples[0, :] = q_init.theta
 
 
-    def _log_psi_helper(q):
+    q = q_init.clone()
+    def _log_psi_helper(theta):
+        q.theta.copy_(theta)
         q.theta.requires_grad_(True)
         _kl = -q.entropy() - q.monte_carlo_ev(log_p, n_kl_samples)
         _grad_kl = torch.autograd.grad(_kl, q.theta)[0]
@@ -26,38 +28,34 @@ def stams_mvn_langevin(log_p, lam_kl, q_init, n_samples=1000, burn_in=100, n_kl_
 
 
     log_psi = torch.zeros(n_samples + burn_in)
-    log_psi[0], grad_log_psi = _log_psi_helper(q_init)
+    log_psi[0], grad_log_psi = _log_psi_helper(samples[0, :])
 
     # Pre-sample accept/reject thresholds for Metropolis adjustments
     u = torch.rand(n_samples + burn_in).log()
     accept = torch.ones(n_samples + burn_in)
 
-    q = q_init.clone()
     for t in range(1, n_samples + burn_in):
         # Proposed Langevin step
-        new_theta = q.theta + dt * grad_log_psi + sqrt(2*dt)*torch.randn(q.n_params)
+        theta, step_theta = samples[t-1, :], dt * grad_log_psi + sqrt(2*dt)*torch.randn(q_init.n_params)
 
-        # Provisionally assign new_theta to q and evaluate the new point
-        tmp_theta, q.theta = q.theta, new_theta
-        new_log_psi, new_grad_log_psi = _log_psi_helper(q)
+        # Evaluate the new point
+        new_log_psi, new_grad_log_psi = _log_psi_helper(theta + step_theta)
 
         # Compute (log) Metropolis ratio, log[p(x')q(x|x')/p(x)q(x'|x)]
-        log_q_forward = -1 / (4*dt) * torch.sum((new_theta - (tmp_theta + dt*grad_log_psi))**2)
-        log_q_reverse = -1 / (4*dt) * torch.sum((tmp_theta - (new_theta + dt*new_grad_log_psi))**2)
-        log_metropolis_ratio =  new_log_psi - log_psi[t-1] + log_q_reverse - log_q_forward
+        log_q_forward = -1 / (4*dt) * torch.sum((step_theta - dt*grad_log_psi)**2)
+        log_q_reverse = -1 / (4*dt) * torch.sum((step_theta + dt*new_grad_log_psi)**2)
+        log_metropolis_ratio = new_log_psi - log_psi[t-1] + log_q_reverse - log_q_forward
 
         # Accept or reject
         if log_metropolis_ratio > u[t]:
             accept[t] = 1.
             log_psi[t] = new_log_psi
             grad_log_psi = new_grad_log_psi
+            samples[t, :] = theta + step_theta
         else:
             accept[t] = 0.
-            q.theta = tmp_theta  # Undo provisional update to q from above
             log_psi[t] = log_psi[t-1]
-
-        # Store sample
-        samples[t, :] = q.theta
+            samples[t, :] = theta
 
     # Return a dict containing samples plus other useful metadata
     return {
