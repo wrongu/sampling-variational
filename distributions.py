f44f82fe18bdfebd3b8eccd767d9168b268f49fd
wrongu <richard.d.lange.13@dartmouth.edu>
2021-09-15 09:37:21 -0400

    1d mixture of laplace in distributions.py

--- distributions.py
+++ distributions.py
@@ -15,25 +15,39 @@ def log_prob_cigar(xy, c=.99):
     return -0.5*(a*x*x + a*y*y + 2*b*x*y)
 
 
+def log_prob_mix_laplace(x, means=(-1.5, 1.5), weights=(0.4, 0.6), scales=(0.75, 0.75)):
+    means, weights, scales = torch.tensor(means), torch.tensor(weights), torch.tensor(scales)
+    mode_log_probs = -torch.abs((x.view(-1, 1) - means.view(1, -1)) / scales.view(1, -1)) - torch.log(2 * scales)
+    log_probs = torch.logsumexp(torch.log(weights.view(1, -1)) + mode_log_probs, dim=1)
+    return log_probs.reshape(x.size())
+
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
-    x = torch.linspace(-4, 4)
+    x = torch.linspace(-4, 4, 100)
     xx, yy = torch.meshgrid(x, x)
     xy = torch.stack([xx.flatten(), yy.flatten()], dim=0)
 
-    def plot_helper(log_prob_fn):
+    def plot_helper_1d(log_prob_fn):
+        p = log_prob_fn(x).exp()
+        plt.plot(x, p)
+        plt.xlim([min(x), max(x)])
+
+    def plot_helper_2d(log_prob_fn):
         lp = log_prob_fn(xy)
         p = (lp - lp.max()).exp().reshape(xx.size())
         plt.contourf(xx, yy, p, origin='lower')
         plt.xlim([min(x), max(x)])
         plt.ylim([min(x), max(x)])
 
-    plt.figure(figsize=(8, 4))
-    plt.subplot(1, 2, 1)
-    plot_helper(log_prob_banana)
+    plt.figure(figsize=(8, 8))
+    plt.subplot(2, 2, 1)
+    plot_helper_2d(log_prob_banana)
     plt.title('banana')
-    plt.subplot(1, 2, 2)
-    plot_helper(log_prob_cigar)
+    plt.subplot(2, 2, 2)
+    plot_helper_2d(log_prob_cigar)
     plt.title('cigar')
+    plt.subplot(2, 2, 3)
+    plot_helper_1d(log_prob_mix_laplace)
+    plt.title('mix laplace')
     plt.show()
